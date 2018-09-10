3645bc95a0e3a09ae5347d63a29ef352ce3216a6
AlisG26 <apgb1996@gmail.com>
2018-09-10 18:49:47 -0500

    creacion proyecto empleados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EditEmployeeComponent } from './employees/edit-employee/edit-employee.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'employees', component: EmployeesComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EditEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
